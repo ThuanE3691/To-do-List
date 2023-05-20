7a1e4a441204288648a947fd6d12e2c605756725
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-05-20 15:55:02 +0700

    Make Login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,7 +7,6 @@ const User = require("../models/User.js");
 
 
 /// @POST REGISTER USER
-
 router.post("/register", async (req, res) => {
 	const { username, password } = req.body;
 
@@ -47,7 +46,7 @@ router.post("/register", async (req, res) => {
 			{
 				user_id: newUser._id,
 			},
-			process.env.ACCESS_TOKEN
+			process.env.ACCESS_TOKEN_SECRET
 		);
 
 		res.status(200).json({
@@ -56,12 +55,66 @@ router.post("/register", async (req, res) => {
 			accessToken: accessToken,
 		});
 	} catch (error) {
+        console.log(error);
 		res.status(404).json({
 			success: false,
 			message: "Internal Server",
-			error: error,
+			error: error.message,
 		});
 	}
 });
+
+
+///@POST LOGIN 
+
+router.post('/login',async (req, res) => {
+    const { username, password } = req.body;
+
+	// Check if the user has fill username and password
+	if (!username || !password) {
+		return res.status(400).json({
+			success: false,
+			message: "Missing username or password",
+		});
+	}
+
+    try {
+        // Find the user 
+        const user = await User.findOne({username: username})
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "Your username or password is incorrect"
+            })
+        }
+
+        // User founded
+        const passwordValid = await argon2.verify(user.password, password)
+        if (!passwordValid){
+            return res.status(404).json({
+                success: false,
+                message: "Your username or password is incorrect"
+            })
+        }
+
+        const accessToken = jwt.sign({user_id: user._id}, process.env.ACCESS_TOKEN_SECRET)
+        res.json({
+            success: true,
+            message: 'User logged in successfully',
+            accessToken: accessToken
+        })
+
+
+    } catch (error) {
+        console.log(error);
+		res.status(404).json({
+			success: false,
+			message: "Internal Server",
+			error: error.message,
+		});
+    }
+})
+
 
 module.exports = router;
