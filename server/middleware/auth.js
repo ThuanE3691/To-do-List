a7fa7775e4595a1c6f542a32d4c51a9bbe20ca1a
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-05-20 16:27:53 +0700

    Add verify token

---
+++ server/middleware/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+	const authHeader = req.headers("Authorization");
+
+	const token = authHeader && authHeader.split(" ")[1];
+
+	if (!token) {
+		return res.status(401).json({
+			success: false,
+			message: "Access token not found",
+		});
+	}
+
+	try {
+		const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+		req.user_id = decoded.user_id;
+		next();
+	} catch (error) {
+		console.log(error);
+		return res.status(403).json({
+			success: false,
+			message: "Invalid Token",
+		});
+	}
+};
+
+module.exports = verifyToken;
